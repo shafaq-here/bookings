930f9c89b9971a396641363223752b2416eca6be
shafaq-here <shafaqisrail@gmail.com>
2023-03-16 00:04:31 +0530

    Added all the necessary templates, their routes and respective handlers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,6 +22,11 @@ func routes(a *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.HomeHandler)
 	mux.Get("/about", handlers.Repo.AboutHandler)
 	mux.Get("/sections", handlers.Repo.SectionsHandler)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
 
 	fileServer := http.FileServer(http.Dir("./static"))
 
